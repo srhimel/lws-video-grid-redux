0c584801947e37fb6b17c585592d1a80004f3f97
srhimel <himusr@gmail.com>
2022-10-20 18:03:49 +0600

    added filter slice

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,14 @@ import videosReducer from '../features/videos/videosSlice'
 import tagsReducer from '../features/tags/tagsSlice'
 import videoReducer from '../features/video/VideoSlice'
 import relatedVideosReducer from '../features/relatedVideos/relatedVideosSlice'
+import filterReducer from '../features/filter/filterSlice'
 
 export const store = configureStore({
   reducer: {
     videos: videosReducer,
     tags: tagsReducer,
     video: videoReducer,
-    relatedVideos: relatedVideosReducer
+    relatedVideos: relatedVideosReducer,
+    filter: filterReducer
   }
 })
--- src/views/components/Tags/Tag.js
+++ src/views/components/Tags/Tag.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { tagRemoved, tagSelected } from '../../../features/filter/filterSlice'
 
 const Tag = ({ tag }) => {
+  const { title } = tag
+  const dispatch = useDispatch()
+  const { tags: selectedTags } = useSelector((state) => state.filter)
+  const isSelected = selectedTags.includes(title)
+
+  const handleSelect = () => {
+    isSelected ? dispatch(tagRemoved(title)) : dispatch(tagSelected(title))
+  }
   return (
-    <div className='bg-blue-200 text-blue-800 px-4 py-1 rounded-full cursor-pointer'>
-      {tag.title}
+    <div
+      className={`${
+        isSelected ? 'bg-blue-600 text-white' : 'bg-blue-200 text-blue-800'
+      } px-4 py-1 rounded-full cursor-pointer`}
+      onClick={handleSelect}>
+      {title}
     </div>
   )
 }
